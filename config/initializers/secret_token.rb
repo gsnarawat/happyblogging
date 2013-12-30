15bc2c07d23255ca0740705d3edee5db93877c48
gsnarawat <gopalnarawat@gmail.com>
2013-12-30 18:22:25 +0530

    Just a fine Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OwnProject::Application.config.secret_key_base = 'bf8a15e1b4a4f430db002318462d1c888cf3f096368e54e2dbb41c3a7eaa36be8d051de59c067aa7aede66781f1809e696c8d2b6bec53250499b765a1a26b6ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OwnProject::Application.config.session_store :cookie_store, key: '_own_project_session'
